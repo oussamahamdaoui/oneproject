fda3759c79342b3288d358218a823db54bae02c6
oussama hamdaoui <oussamahamdaoui@live.fr>
2019-06-27 13:38:48 +0200

    feat: change locked icon

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,24 +6,21 @@ const ProjectCard = ({
   const DomElement = html`
     <article>
       <img src="${img}">
-      <h1>${isOpen ? '' : '<i class="icofont-shield-alt"></i>'} ${name}</h1>
+      <h1>${isOpen ? '' : '<i class="icofont-lock"></i>'} ${name}</h1>
       <p class="team">
         ${team.map(e => html`<strong class='${e.author ? 'author' : ''}'>@${e.name}</strong>`)}
       </p>
       <p>
         ${description}
       </p>
-
       <p class="tags">
         ${tags.map(tag => html`<strong>${tag}</strong>`)}
       </p>
-      
       <ul>
         <li class="join"><i class="icofont-hand-power"></i> Join</li>
         <li class="like"><i class="icofont-heart"></i> Like</li>
         <li class="save"><i class="icofont-book-mark"></i> Save</li>
       </ul>
-
     </article>
   `;
 
